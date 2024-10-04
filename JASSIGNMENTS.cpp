a5c20a05018aeb635e541b8b4c3ac0790c449cbd
vaibhav singh chandel <143792306+vaibhavchandel12@users.noreply.github.com>
2024-10-04 10:03:52 +0530

    Add files via upload

---
+++ JASSIGNMENTS.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    int s=x+3;
+	    if(s<=10){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	    
+	}
+	return 0;
+
+}
