a3784df1ab321718a14fc2bc392da72ba8fa0e72
vaibhav singh chandel <143792306+vaibhavchandel12@users.noreply.github.com>
2024-11-12 21:29:24 +0530

    Add files via upload

---
+++ BATH.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int t=2*y;
+	    
+	    cout<<x/t<<endl;
+	}
+return 0;
+}
