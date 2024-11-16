3ce8534009e08a767c560390c6134cda49af7da3
vaibhav singh chandel <143792306+vaibhavchandel12@users.noreply.github.com>
2024-11-16 10:12:38 +0530

    Add files via upload

---
+++ PRESENTS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int N;
+	    cin>>N;
+	    int t=((N/5)*4)+(N%5);
+	    cout<<t<<endl;
+	}
+return 0;
+}
+
